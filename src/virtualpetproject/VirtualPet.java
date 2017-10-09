4defd99381e4e88153002915c5310591273c832e
Jonathan Ohueri <jonathan.ohueri@gmail.com>
2017-10-09 09:49:12 -0400

    Fiddled with the stats

--- src/virtualpetproject/VirtualPet.java
+++ src/virtualpetproject/VirtualPet.java
@@ -2,4 +2,49 @@ package virtualpetproject;
 
 public class VirtualPet {
 
+	// Instance Data
+	private int hunger; // instance variable
+	private int boredom;
+	private int affection;
+	private int bathroom;
+	private int thirst;
+
+	// Constructor
+	public VirtualPet(int hunger, int boredom, int affection, int bathroom, int thirst) {
+		this.hunger = hunger;
+		this.boredom = boredom;
+		this.affection = affection;
+		this.bathroom = bathroom;
+		this.thirst = thirst;
+	}
+
+	// Method of Behavior
+	public void tick(int time) {
+		hunger += time;
+		boredom += time;
+		affection += time;
+		bathroom += time;
+		thirst +=time;
+	}
+
+	public void feedPet(int food) {
+		hunger -= food;
+		thirst += (food/2);
+	}
+
+	public void playPet(int play) {
+		boredom -= play;
+	}
+
+	public void rubPet(int rub) {
+		affection -= rub;
+	}
+
+	public void letOut(int poop) {
+		bathroom -= poop;
+	}
+	public void giveWater(int water) {
+		thirst -= water;
+		bathroom += (water/3);
+	}
 }
--- src/virtualpetproject/VirtualPetApp.java
+++ src/virtualpetproject/VirtualPetApp.java
@@ -1,10 +1,78 @@
 package virtualpetproject;
 
+import java.util.Scanner;
+
+// Assign values to hunger and the different attributes. If hunger gets below a certain value then they are hungry. 
+//
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		
+		Scanner input = new Scanner(System.in); 
+		VirtualPet clifford = new VirtualPet(60, 60, 60, 60, 60);
+		
+		System.out.println("You've been tasked with taking care of Clifford the Big Red Dog.");
+		System.out.println("He's a national treasure...don't screw this up.\n");
+		System.out.println(" ");
+		System.out.println("Good luck. If you fail you'll be know as that guy who killed Cliford the Big Red Dog. ");
+		String choice;
+		
+		do { 
+			System.out.println("Choose and option: ");
+			System.out.println("Press 1 to feed Clifford");
+			System.out.println("Press 2 to play with Clifford");
+			System.out.println("Press 3 to pet Clifford on his big red belly");
+			System.out.println("Press 4 to let Clifford out to relieve himself and ruin someone's day.");
+			System.out.println("Press 5 to let Clifford out to get a drink from a large body of water. ");
+			System.out.println("Press 6 to abandon Clifford.");
+			choice = input.nextLine();
+			clifford.tick(1);
+			
+		if(choice.equals("1")) { 
+			System.out.println("Clifford get's a bite to eat thanks to contributions from ten local farmers.");
+			clifford.feedPet(10);
+			
+			
+		}  else if (choice.equals("2")) {
+			System.out.println("You ride Clifford's back for a short jaunt around the neighborhood.");
+			System.out.println("You end up 10 miles from home in 5 minutes thanks to his large strides.");
+			System.out.println(" ");
+			clifford.playPet(10);
+			
+		}else if (choice.equals("3")) {
+			System.out.println("You pet Clifford on his big red belly. He wags his tail profusely which blows the roof off of your house.");
+			System.out.println(" ");
+			clifford.rubPet(10);
+			
+			
+		} else if (choice.equals("4")) {
+			System.out.println("Clifford goes outside to relieve himself. Poop and pee are everywhere...");
+			System.out.println("EVERYWHERE!!");
+			System.out.println(" ");
+			clifford.letOut(10);
+			
+		}else if (choice.equals("5")) {
+			System.out.println("Clifford gives you a ride to the pond and he drinks 10% of its volume.");
+			System.out.println("The environmentalist in you dies a little with each massive gulp. ");
+			System.out.println(" ");
+			clifford.giveWater(10);
+			
+		}else if (choice.equals("6")) {
+			System.out.println("You didn't have any business owning a large animal anyway.");
+			System.out.println("Your neighborhood association is actively looking to get you evicted.");
+			System.out.println("And there is SO MUCH POOP. What were you thinking?! Just go to bed.");
+			System.out.println("Good bye.");
+			break;
+		}
+		System.out.println("What else would you like to do?");
+		System.out.println(" ");
+	
+		
+		}while (!choice.equals("6"));
+		
+		}
+		
+		
+	
 }
+	
